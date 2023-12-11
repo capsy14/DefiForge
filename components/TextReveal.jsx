3b3958e77bd98fc528d3dc5b90571297c32ca560
mohammedasimahmed <asim60999@gmail.com>
2023-12-11 15:32:09 +0530

    made white circle appearing effect better in loadup effect

--- components/TextReveal.jsx
+++ components/TextReveal.jsx
@@ -20,11 +20,24 @@ const RevealTextAnimation = ({ customText }) => {
           }
         }
         setTimeout(() => {
-          // document.getElementById("span 6").innerText = "";
+          document.getElementById("span 6").innerText = "";
+          document.getElementById("span 6").style.width = "100vw";
+          document.getElementById("span 6").style.height = "100vh";
           const CircleDiv = document.createElement("div");
-          CircleDiv.classList.add("growing-circle");
+          CircleDiv.id = "circleGrowing";
+          CircleDiv.classList.add("opacity_transition");
+          //   CircleDiv.classList.add("growing-circle");
+          //   CircleDiv.classList.add("opacity_transition");
           document.getElementById("span 6").appendChild(CircleDiv);
-          document.getElementById("span 6").classList.add("growing-circle");
+          setTimeout(() => {
+            CircleDiv.style.opacity = 1;
+            setTimeout(() => {
+              CircleDiv.classList.add("growing-circle");
+            }, 1000);
+          }, 10);
+          //   document
+          //     .querySelector("#circleGrowing")
+          //     .classList.add("opacity_transition");
         }, 1000);
       }, 1000);
     }, 1100);
--- files/Home.jsx
+++ files/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
       setTimeout(() => {
         setVal(true);
       }, 700);
-    }, 3700);
+    }, 4650);
   }, []);
   const DefiForge = lazy(() => import("@/components/DefiForge"));
   return (
