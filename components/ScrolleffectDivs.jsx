d2453a20a584a8a26a8f2b1a3fdf2bdb9a4f062f
mohammedasimahmed <asim60999@gmail.com>
2023-12-13 16:46:29 +0530

    minor fixing

--- components/ScrolleffectDivs.jsx
+++ components/ScrolleffectDivs.jsx
@@ -4,22 +4,25 @@ import TextReveal from "./TextReveal";
 const ScrolleffectDivs = () => {
   return (
     <>
-      <div className="w-screen h-screen  flex flex-col relative">
+      <div
+        className="w-screen flex flex-col relative overflow-y-auto"
+        style={{ height: "90vh" }}
+      >
         <div className="flex justify-center items-center">
           <TextReveal customText={"Savor The Expirience"} val={false} />
         </div>
-        <div className="flex-1  overflow-y-auto overflow-x-hidden scroll-snap">
-          <div className=" h-full w-screen scroll-snap-align  ml-8 z-10">
+        <div className="flex-1 overflow-y-auto overflow-x-hidden scroll-snap">
+          <div className=" h-full w-screen scroll-snap-align  ml-8">
             <TextReveal customText={"Savor The Expirience"} val={false} />
           </div>
-          <div className="h-full w-screen scroll-snap-align ml-8 z-10">
+          <div className="h-full w-screen scroll-snap-align ml-8">
             <TextReveal customText={"Savor The Expirience"} val={false} />
           </div>
-          <div className="h-full w-screen scroll-snap-align ml-8 z-10">
+          <div className="h-full w-screen scroll-snap-align ml-8">
             <TextReveal customText={"Savor The Expirience"} val={false} />
           </div>
         </div>
-        <div className="absolute top-36 right-12 bg-cyan-400 h-[300px] w-[300px] z-0"></div>
+        <div className="absolute top-36 right-12 bg-cyan-400 h-[300px] w-[300px]"></div>
       </div>
     </>
   );
