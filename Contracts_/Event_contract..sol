2fdcb4c161e226bc367f1e80e15ec83df414e457
Divyansh Gupta <iit2022018@iiitl.ac.in>
2023-12-13 15:00:38 +0530

    feat(contracts_): add new data types,get and change status feature
    get_event_status and change_event_status function added , new data types related to tickets
    introduced

--- Contracts_/Event_contract..sol
+++ Contracts_/Event_contract..sol
@@ -10,18 +10,20 @@ contract Event_contract {
     string public Event_location;
     string public Event_date;
     address public Event_manager_address;
-    bool Event_is_live;
-    uint256 no= 65;
+    bool public Event_is_live;
+
+    mapping(uint256 => uint256) public Total_tickets;
+    mapping(uint256 => uint256) public Tickets_remaining;
+    uint256[] public ticet_prices;
 
     constructor(
-        string  memory _event_name,
+        string memory _event_name,
         string memory _event_discription,
         string memory _event_comapany_organiser,
         string memory _event_manager,
         string memory _event_location,
         string memory _event_date,
-        address _event_manager_address        
-
+        address _event_manager_address
     ) {
         Event_name = _event_name;
         Event_discription = _event_discription;
@@ -32,5 +34,8 @@ contract Event_contract {
         Event_manager_address = _event_manager_address;
         Event_is_live = true;
     }
-    
+
+    function change_event_status() {
+        Event_is_live = false;
+    }
 }
--- Contracts_/Event_factory.sol
+++ Contracts_/Event_factory.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "./Event_contract.sol";
+
 contract Event_factory {
     // array containing address of all the events contract->
     Event_contract[] public Event_array;
@@ -24,18 +25,19 @@ contract Event_factory {
     ) external payable pay_for_function {
         Event_contract event_contract = new Event_contract(
             _event_name,
-             _event_discription,
+            _event_discription,
             _event_comapany_organiser,
             _event_manager,
             _event_location,
             _event_date,
             msg.sender
-        
         );
 
         Event_array.push(event_contract);
-        // address_to_event[address(_event)]= _event;
     }
 
-    
+    function get_event_status(address _event_address) private returns (bool) {
+        Event_contract _event = Event_contract(_event_address);
+        return (_event.Event_is_live);
+    }
 }
