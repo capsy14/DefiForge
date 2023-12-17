93a82dceb7a6a2a272a5be25ab527fcd28c4bc39
mohammedasimahmed <asim60999@gmail.com>
2023-12-17 11:33:55 +0530

    updates in ImagesReveal.jsx

--- components/ImagesReveal.jsx
+++ components/ImagesReveal.jsx
@@ -10,7 +10,7 @@ const ImagesReveal = () => {
   const ref5 = useRef(null);
   const ref6 = useRef(null);
   const [displayIt, setDisplayIt] = useState(false);
-  const scrolleffectStart = useRef(0)
+  const scrolleffectStart = useRef(0);
   const [move, setMove] = useState(false);
   const controls = useAnimation();
   const [ref, inView] = useInView();
@@ -46,16 +46,16 @@ const ImagesReveal = () => {
     ) {
       setTimeout(() => {
         ref1.current.style.opacity = 1;
-        ref1.current.style.transform = "translateX(50px)";
+        ref1.current.style.transform = "translateX(0px)";
         ref2.current.style.opacity = 1;
-        ref2.current.style.transform = "translateX(350px)";
+        ref2.current.style.transform = "translateX(0px)";
         ref3.current.style.opacity = 1;
-        ref3.current.style.transform = "translateX(700px)";
+        ref3.current.style.transform = "translateX(0px)";
         ref4.current.style.opacity = 1;
-        ref4.current.style.transform = "translateX(1060px)";
+        ref4.current.style.transform = "translateX(0px)";
         setTimeout(() => {
           setMove(true);
-          scrolleffectStart.current = (window.scrollY + 50)
+          scrolleffectStart.current = window.scrollY + 50;
         }, 4400);
         // ref5.current.style.opacity = 1;
         // ref5.current.style.transform = "translateX(850px)";
@@ -66,34 +66,34 @@ const ImagesReveal = () => {
   }, [displayIt]);
 
   function handleScroll() {
-    console.log("scrolled how much", window.scrollY );
+    console.log("scrolled how much", window.scrollY);
     if (window.scrollY <= 1960) {
       ref5.current.style.opacity = 0;
       ref6.current.style.opacity = 0;
-      ref5.current.style.transform = `translateY(${0}px) translateX(${850}px)`;
-      ref6.current.style.transform = `translateY(${0}px) translateX(${850}px)`;
-    } else if (window.scrollY > 1960 && window.scrollY < 1960 + 430) {
+      ref5.current.style.transform = `translateY(${0}px) translateX(${0}px)`;
+      ref6.current.style.transform = `translateY(${0}px) translateX(${0}px)`;
+    } else if (window.scrollY > 1960 && window.scrollY < 1960 + 800) {
       ref5.current.style.opacity = 1;
       ref6.current.style.opacity = 0;
       ref5.current.style.transitionDelay = "0ms";
       ref5.current.style.transitionDuration = "50ms";
       ref5.current.style.transform = `translateY(${
-        1.4 * (window.scrollY - 1960)
-      }px) translateX(${850 - 2.1 * (window.scrollY - 1960)}px)`;
+        1 * (window.scrollY - 1960)
+      }px) translateX(${0 - 1.5 * (window.scrollY - 1960)}px)`;
       ref6.current.style.transitionDelay = "0ms";
       ref6.current.style.transitionDuration = "50ms";
       ref6.current.style.transform = `translateY(${
-        1.4 * (window.scrollY - 1960)
-      }px) translateX(${850 - 2.1 * (window.scrollY - 1960)}px)`;
-    } else if (window.scrollY > 1960 + 500 && window.scrollY < 1960+505) {
+        1.2 * (window.scrollY - 1960)
+      }px) translateX(${0 - 1.8 * (window.scrollY - 1960)}px)`;
+    } else if (window.scrollY > 1960 + 800 && window.scrollY < 1960 + 805) {
       ref6.current.style.opacity = 0;
-    } else if (window.scrollY > 1960 + 505 && window.scrollY < 1960 + 1070) {
+    } else if (window.scrollY > 1960 + 805 && window.scrollY < 1960 + 1530) {
       ref6.current.style.opacity = 1;
       ref6.current.style.transitionDelay = "0ms";
       ref6.current.style.transitionDuration = "50ms";
       ref6.current.style.transform = `translateY(${
-        1.2 * (window.scrollY - 1960 )
-      }px) translateX(${120 + 1.3 * (window.scrollY - 1960 - 505)}px)`;
+        0 + 1.1 * (window.scrollY - 1960)
+      }px) translateX(${-1370 + 1.2 * (window.scrollY - 1960 - 505)}px)`;
     }
   }
 
@@ -105,11 +105,11 @@ const ImagesReveal = () => {
     <div className="mt-28">
       <motion.div ref={ref} animate={controls} {...animationOptions}>
         {displayIt && (
-          <div className="w-screen relative">
+          <div className="w-screen relative flex justify-center items-center flex-wrap">
             <div
               ref={ref1}
-              className="absolute p-9 bg-transparent h-[350px] w-[350px] opacity-0 transition duration-1000 "
-              style={{transform:"translateX(-50px)"}}
+              className=" p-9 bg-transparent h-[350px] w-[350px] opacity-0 transition duration-1000 "
+              style={{ transform: "translateX(-50px)" }}
             >
               <img
                 src="/images/_extensible.png"
@@ -119,10 +119,10 @@ const ImagesReveal = () => {
             </div>
             <div
               ref={ref2}
-              className="absolute p-9 bg-transparent h-[350px] w-[350px] opacity-0 transition translate-x-72 duration-1000 "
+              className=" p-9 bg-transparent h-[350px] w-[350px] opacity-0 transition duration-1000 "
               style={{
                 transitionDelay: "1100ms",
-                transform: "translateX(150px)",
+                transform: "translateX(-50px)",
                 transitionDuration: "1100ms",
               }}
             >
@@ -130,10 +130,10 @@ const ImagesReveal = () => {
             </div>
             <div
               ref={ref3}
-              className="absolute p-9 bg-transparent h-[350px] w-[350px] opacity-0 transition duration-1000 "
+              className=" p-9 bg-transparent h-[350px] w-[350px] opacity-0 transition duration-1000 "
               style={{
                 transitionDelay: "2200ms",
-                transform: "translateX(500px)",
+                transform: "translateX(-50px)",
                 transitionDuration: "1100ms",
               }}
             >
@@ -145,10 +145,10 @@ const ImagesReveal = () => {
             </div>
             <div
               ref={ref4}
-              className="absolute p-9 bg-transparent h-[350px] w-[350px] opacity-0 z-20"
+              className="relative p-9 bg-transparent h-[350px] w-[350px] opacity-0 z-20"
               style={{
                 transitionDelay: "3300ms",
-                transform: "translateX(900px)",
+                transform: "translateX(-50px)",
                 transitionDuration: "1100ms",
               }}
             >
@@ -157,33 +157,41 @@ const ImagesReveal = () => {
                 alt=""
                 className="w-full h-full"
               />
-            </div>
-            <div
-              ref={ref5}
-              className="absolute bg-transparent h-[450px] w-[750px] opacity-0 z-10"
-              style={{
-                transitionDelay: "3300ms",
-                transform: "translateX(850px)",
-                transitionDuration: "1100ms",
-              }}
-            >
-              <img src="/images/sphere.gif" alt="" className="w-full h-full" />
-            </div>
-            <div
-              ref={ref6}
-              className="absolute p-9 bg-transparent h-[450px] w-[450px] opacity-0 z-0"
-              style={{
-                transitionDelay: "3300ms",
-                transform: "translateX(850px)",
-                transitionDuration: "1100ms",
-              }}
-            >
-              <img src="/images/crystal.gif" alt="" className="w-full h-full" />
+              <div
+                ref={ref5}
+                className="absolute bg-transparent h-[450px] w-[600px] opacity-0 z-10 top-0 left-0"
+                style={{
+                  transitionDelay: "3300ms",
+                  // transform: "translateX(850px)",
+                  transitionDuration: "1100ms",
+                }}
+              >
+                <img
+                  src="/images/sphere.gif"
+                  alt=""
+                  className="w-full h-full"
+                />
+              </div>
+              <div
+                ref={ref6}
+                className="absolute bg-transparent h-full w-full opacity-0 z-0 top-0 left-0"
+                style={{
+                  transitionDelay: "3300ms",
+                  // transform: "translateX(850px)",
+                  transitionDuration: "1100ms",
+                }}
+              >
+                <img
+                  src="/images/crystal.gif"
+                  alt=""
+                  className="w-full h-full"
+                />
+              </div>
             </div>
           </div>
         )}
       </motion.div>
-      <div className="w-screen h-screen flex flex-col-reverse items-end translate-y-56">
+      <div className="w-screen h-screen flex flex-col-reverse items-end translate-y-24">
         <div className=" text-3xl w-1/2">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum
           <br />
